5af9a258451c4271f19240759ffcbbf980f5e7ce
d.letovaltsev <daniil-letovalcev@yandex.ru>
2022-09-23 21:25:03 +0500

    init

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import bridge from "@vkontakte/vk-bridge";
 import App from "./App";
+import { RouterContext } from "@happysanta/router";
+import { router } from "./router/router";
 
 // Init VK  Mini App
 bridge.send("VKWebAppInit");
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<RouterContext.Provider value={router}><App /></RouterContext.Provider>, document.getElementById("root"));
 if (process.env.NODE_ENV === "development") {
   import("./eruda").then(({ default: eruda }) => {}); //runtime download
 }
